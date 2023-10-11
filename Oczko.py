f1d5a3ead24c14ddb9fbc028e2e7d6845917571a
Kasperkiewicz <144849163+Kasper74@users.noreply.github.com>
2023-10-11 17:31:47 +0200

    Oczko.py

--- Oczko.py
+++ Oczko.py
@@ -6,7 +6,7 @@ liczbakart = int(input("Podaj liczbę kart: "))
 # Dane o kartach
 mozliwe_karty = [4, 3, 2, 10, 11]
 karty_gracza = []
-
+karty_przeciwnika = random.randint(3, 35)
 # Tworzenie kart
 for i in range(liczbakart):
   nowa_karta = random.choice(mozliwe_karty)
@@ -14,12 +14,26 @@ for i in range(liczbakart):
 
 # Obliczanie sumy wszystkich kart
 sumaLiczb = sum(karty_gracza)
-print(f"Twoje liczby: {karty_gracza}")
+print(f"Twoja suma: {sumaLiczb}")
+print(f"Suma przeciwnika: {karty_przeciwnika}")
+# Wysyłanie wiadomości o Wygranej / Przegranej
+if karty_przeciwnika == 21:
+   print("Twój przeciwnik wygrał!")
 
-# Wysyłanie wiadomości, o wygranej / przegranej
-if sumaLiczb > 21:
-   print(f"Przegrałeś! Suma twoich liczb to {sumaLiczb}")
-elif sumaLiczb < 21:
-   print(f"Przegrałeś! Suma twoich liczb to {sumaLiczb}")
+
+if  sumaLiczb == karty_przeciwnika or (sumaLiczb > 21 and karty_przeciwnika > 21):
+    print("Remis! ")
+
+elif sumaLiczb <= 21 and karty_przeciwnika > 21:
+    print("Wygrałeś")
+  
+elif karty_przeciwnika <= 21 and sumaLiczb > 21:
+   print("Przegrałeś!")
+
+elif karty_przeciwnika < 21 and sumaLiczb < 21:
+    if sumaLiczb > karty_przeciwnika:
+      print("Wygrałeś!")
+    else:
+      print("Przegrałeś!")
 else:
-   print(f"Wygrałeś! Suma twoich liczb to {sumaLiczb}")
+    print("Wystąpił błąd!")

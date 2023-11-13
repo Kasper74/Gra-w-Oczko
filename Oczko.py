f1a3fdef9c65b72205f85580ce61802fe05f0082
Kasperkiewicz <144849163+Kasper74@users.noreply.github.com>
2023-11-13 11:31:18 +0100

    Oczko.py

--- Oczko.py
+++ Oczko.py
@@ -2,38 +2,55 @@ import random
 
 # Odbieranie danych
 liczbakart = int(input("Podaj liczbę kart: "))
-
+odpowiedz = ""
+sumaLiczb = 0
 # Dane o kartach
 mozliwe_karty = [4, 3, 2, 10, 11]
 karty_gracza = []
 karty_przeciwnika = random.randint(3, 35)
+
 # Tworzenie kart
 for i in range(liczbakart):
-  nowa_karta = random.choice(mozliwe_karty)
-  karty_gracza.append(nowa_karta)
+    nowa_karta = random.choice(mozliwe_karty)
+    karty_gracza.append(nowa_karta)
 
-# Obliczanie sumy wszystkich kart
+print(f"Twoje karty: {karty_gracza}")
+
+odpowiedz = input("Czy chcesz kontynuować losowanie kart?: ")
+while odpowiedz.lower() == "tak":
+    liczbakart = int(input("Ile kart chcesz wylosować?: ")) 
+    # Tworzenie kart
+    for i in range(liczbakart):
+        nowa_karta = random.choice(mozliwe_karty)
+        karty_gracza.append(nowa_karta)
+    print(f"Twoje karty: {karty_gracza}")
+    odpowiedz = input("Czy chcesz kontynuować losowanie kart?: ")
+
 sumaLiczb = sum(karty_gracza)
-print(f"Twoja suma: {sumaLiczb}")
-print(f"Suma przeciwnika: {karty_przeciwnika}")
+
+# Wypisywanie danych
+print(f"Twoje Liczby: {karty_gracza}")
+print(f"Twoja Suma: {sumaLiczb}")
+print(f"Suma Przeciwika {karty_przeciwnika}")
 
 # Wysyłanie wiadomości o Wygranej / Przegranej
 if karty_przeciwnika == 21:
-   print("Twój przeciwnik wygrał!")
+    print("Twój przeciwnik wygrał!")
+
 
 if  sumaLiczb == karty_przeciwnika or (sumaLiczb > 21 and karty_przeciwnika > 21):
     print("Remis!")
 
 elif sumaLiczb <= 21 and karty_przeciwnika > 21:
-    print("Wygrałeś!")
+    print("Wygrałeś")
   
 elif karty_przeciwnika <= 21 and sumaLiczb > 21:
    print("Przegrałeś!")
 
 elif karty_przeciwnika < 21 and sumaLiczb < 21:
     if sumaLiczb > karty_przeciwnika:
-      print("Wygrałeś!")
+        print("Wygrałeś!")
     else:
-      print("Przegrałeś!")
+        print("Przegrałeś!")
 else:
     print("Wystąpił błąd!")
